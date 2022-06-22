2ea0a4607f15b123c15541bc2d44f45a0fc66c23
Red <seryprk@gmail.com>
2022-06-22 10:24:09 +0900

    💎feat : [CH1] Theater 코드 구현

---
+++ ObjectBook/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  ObjectBook
+//
+//  Created by 박세리 on 2022/06/22.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
